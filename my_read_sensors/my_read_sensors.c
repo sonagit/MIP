311bae023dff1cce1effb3ee0e99af26d79a396f
Stu <supadupastu@gmail.com>
2016-11-23 10:07:00 -0800

    hw5

--- my_read_sensors/my_read_sensors.c
+++ my_read_sensors/my_read_sensors.c
@@ -12,8 +12,27 @@
 int initialize_imu_dmp(imu_data_t *data, imu_config_t imu_config);
 int set_imu_interrupt_func(int (*func)(void));
 int stop_imu_interrupt_func();
-
+int print_data(); //prints data to console
 imu_data_t data; //struct to hold new data
+
+// IMU interrupt function that prints to console
+int print_data(){
+	printf("\r");
+
+	// Print accelerometer data
+    if(read_accel_data(&data)<0){
+		printf("read_accel_data failed\n");
+	}
+    printf("%6.2f %6.2f %6.2f   |",	data.accel[0],\
+									data.accel[1],\
+            						data.accel[2]);
+	float g_y = data.accel[1]-0.1;  // Y direction is 0.1 too high
+	float g_z = data.accel[2]-0.45; // Z direction is 0.45 too high
+	float theta = atan2(g_z/9.8,g_y/9.8); // angle to gravity
+	printf("	%6.2f",-theta);
+	
+	fflush(stdout); // flush
+}
 
 int main(){
   
@@ -23,9 +42,9 @@ int main(){
 	  return -1;
 	}
 
-	// default imu configuration
+	// set imu configuration to defaults
 	imu_config_t imu_config = get_default_imu_config();
-	int set_imu_config_to_defaults(imu_config_t *imu_config);
+	//int set_imu_config_to_defaults(imu_config_t *imu_config);
 
 	imu_config.orientation = ORIENTATION_Y_UP;
 	imu_config.dmp_sample_rate = 10;
@@ -36,45 +55,32 @@ int main(){
 		blink_led(RED, 5, 10);
 		return -1;
 	}
-
 	printf("\nReady for some accelerometer data?!\n");
-
+	
 	// Print header for accelerometer data.
 	printf("    Accel XYZ(m/s^2)	|");
 	printf("	Angle to Gravity (rad)");
 	printf("\n");
 	
+	// sets the interrupt function to print data immediately after the header.
+	set_imu_interrupt_func(&print_data);
+	
 	// Keep looping until state changes to EXITING
-	while(get_state()!=EXITING){
-    printf("\r");
-    
-    // Print accelerometer data
-    if(read_accel_data(&data)<0){
-		printf("read_accel_data failed\n");
-		}
-    printf("%6.2f %6.2f %6.2f   |",	data.accel[0],\
-									data.accel[1],\
-            						data.accel[2]);
-	float g_y = data.accel[1]-0.1;  // Y direction is 0.1 too high
-	float g_z = data.accel[2]-0.45; // Z direction is 0.45 too high
-	float theta = atan2(g_z/9.8,g_y/9.8); // angle to gravity
-	printf("	%6.2f",-theta);
-	
-	// Read gyro
-	if(read_gyro_data(&data)<0){
+	while(get_state()!=EXITING){		
+		// Read gyro
+		if(read_gyro_data(&data)<0){
 			printf("read gyro data failed\n");
-	}
-	/*
-	// Integrate gyro data to get absolute position
-	float theta_dot = data.gyro[0]*DEG_TO_RAD; // spin rate
-	float offset = -0.5; // offset
-	
-	// Print gyro data	
-	printf("	%6.1f", theta_dot);
-	*/	
-	
-	fflush(stdout); // flush
-	usleep(100000); // sleep for 0.1 second
+		}
+		/*
+		// Integrate gyro data to get absolute position
+		float theta_dot = data.gyro[0]*DEG_TO_RAD; // spin rate
+		float offset = -0.5; // offset
+		
+		// Print gyro data	
+		printf("	%6.1f", theta_dot);
+		*/	
+		
+		usleep(100000); // sleep for 0.1 second
 	}
 	
 	// exit cleanly
