a3b3d55aba2c84875da7cef764535d45d82f6fa7
Stu <supadupastu@gmail.com>
2016-11-23 11:24:49 -0800

    hw5

--- my_read_sensors/my_read_sensors.c
+++ my_read_sensors/my_read_sensors.c
@@ -17,7 +17,7 @@ int print_data(); //prints data to console
 imu_data_t data; //struct to hold new data from IMU
 float theta_g = 0; // initialize starting angle for euler's method
 float offset = -0.5; // offset of gyro around X axis
-char filename = HW5; // file name for csv
+char filename[] = "HW5"; // file name for csv
 char fp;
 // IMU interrupt function that prints to console
 int print_data(){
