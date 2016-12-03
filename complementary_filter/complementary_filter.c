1845259992a3709c56be534306ae9a8a72682104
Stu <supadupastu@gmail.com>
2016-12-02 20:17:29 -0800

    added sum, fixed file open/write/close

--- complementary_filter/complementary_filter.c
+++ complementary_filter/complementary_filter.c
@@ -1,68 +1,39 @@
 /*******************************************************************************
 * complementary_filter.c
+* By: Stuart Sonatina
 *
-* This is a filter....
+* Calculate theta from accelerometer and gyro data after running them through
+* Low pass and high pass filters, respectively.
+*
 *******************************************************************************/
 
 #include <usefulincludes.h>
 #include <roboticscape.h>
 
 #define SAMPLE_RATE 100
-#define TIME_CONSTANT 1.0
-
+#define TIME_CONSTANT 2.0
 
 // function declarations
 int initialize_imu_dmp(imu_data_t *data, imu_config_t imu_config);
 int set_imu_interrupt_func(int (*func)(void));
-int stop_imu_interrupt_func();
-int print_data(); //prints data to console
+int print_data(); // prints IMU data
 
 // variable declarations
 imu_data_t data; //struct to hold new data from IMU
-float g_y, g_z, theta_a, filtered_theta_a, filtered_theta_g; // gravity, thetas
+float g_y, g_z, theta_a, filtered_theta_a, filtered_theta_g, sum; // gravity, thetas
 float theta_dot, theta_g = 0; // initialize starting angle for euler's method
 float offset = -0.5; // offset of gyro around X axis
-char filename[] = "HW6_Filtered"; // file name for csv
-d_filter_t LP, HP; // Lowpass and Highpass filters
+const float TIME_STEP = 1.0/(float)SAMPLE_RATE; // Calc dt from sample rate
+d_filter_t LP, HP; // Lowpass and Highpass filters structs
+char filename[] = "HW6_Acc-Gyro-Sum"; // file name for csv
+FILE *fp; // Makes a file pointer to stream thing I have no idea really
 
-// IMU interrupt function that prints to console
-int print_data(){
-	printf("\r ");
-
-	// Print accelerometer data
-	printf("%6.2f %6.2f %6.2f   |",	data.accel[0],\
-									data.accel[1],\
-									data.accel[2]);
-	g_y = data.accel[1]-0.1;  // Y direction is 0.1 too high
-	g_z = data.accel[2]-0.45; // Z direction is 0.45 too high
-	theta_a = atan2(-g_z/9.8,g_y/9.8); // angle to gravity
-	
-	// filter high freq noise out of accelerometer data
-	filtered_theta_a = march_filter(&LP,theta_a);
-	printf("        %6.2f      ", filtered_theta_a);
-	
-	// Integrate gyro data to get absolute position
-	theta_dot = (data.gyro[0] - offset)*DEG_TO_RAD; // spin rate in rad
-	theta_g = theta_g + 0.01*theta_dot; // euler's method with t = 0.01
-
-	// filter low freq noise out of gyro data
-	filtered_theta_g = march_filter(&HP,theta_g);
-	// Print angle from gyro data
-	printf("|    %6.1f    ", filtered_theta_g);
-	
-	// print thetas to csv file
-	FILE *fp;
-	fp=fopen(filename,"a"); // open file to append
-	fprintf(fp,"%6.2f,%6.2f\n",filtered_theta_g,filtered_theta_a); // print
-	fclose(fp); // close file
-
-	fflush(stdout); // flush
-	return 0;
-}
 /******************************************************************************
 * int main()
 ******************************************************************************/
 int main(){
+
+	// print welcome
 	printf("\n------------------------------");
 	printf("\n| Welcome to filter madness! |\n");
 	printf("------------------------------\n");
@@ -72,12 +43,14 @@ int main(){
 		printf("ERROR: failed to initialize_cape\n");
 		return -1;
 	}
-	// Calc time step from sample rate
-	const float TIME_STEP = 1.0/(float)SAMPLE_RATE;
-	// get yourself some filters
+    
+	// open file to append thetas to csv
+	fp=fopen(strcat(filename,".csv"),"a"); // opens file to append
+
+    
+    // get yourself some filters
 	LP = create_first_order_lowpass(TIME_STEP, TIME_CONSTANT);
 	HP = create_first_order_highpass(TIME_STEP, TIME_CONSTANT);
-  
 	// reset them filters
 	reset_filter(&LP);
 	reset_filter(&HP);
@@ -95,33 +68,63 @@ int main(){
 		return -1;
 	}
 	
-	// create CSV to record movements
-	printf("\nCreating %s.csv file\n",filename);
-	FILE *fp; // pointer to stream
-	fp=fopen(strcat(filename,".csv"),"w"); // create empty file to write
-	//fprintf(fp,"filtered_theta_g,filtered_theta_a\n"); // print header to file
-	printf("%s.csv file created\n",filename);
-	
-	// print welcome
-	printf("\nReady for some accelerometer data?!\n\n");
-	
 	// Print header to console
 	printf("    Accel XYZ(m/s^2)	|");
 	printf("    theta_g (rad)   |");
-	printf("    theta_a (rad)");
+	printf("    theta_a (rad)   |");
+	printf("      sum  (rad)    |");
 	printf("\n");
 	
-	// sets the interrupt function to print data immediately after the header.
+	// The interrupt function will print data when invoked
 	set_imu_interrupt_func(&print_data);
 	
 	// Keep looping until state changes to EXITING
 	while(get_state()!=EXITING) {
-		usleep(10000); // sleep for 0.01 second
+		usleep(100000); // sleep for 0.1 second
 	}
 	
 	// exit cleanly
 	fclose(fp);
 	power_off_imu();
 	cleanup_cape();
+	return 0;
+}
+
+/******************************************************************************
+* int print_data()
+*
+* IMU interrupt function that prints IMU data to console and csv file
+*
+******************************************************************************/
+int print_data(){
+	printf("\r ");
+
+	// Integrate gyro data to get absolute position of theta
+	theta_dot = (data.gyro[0] - offset)*DEG_TO_RAD; // spin rate in rad
+	theta_g = theta_g + TIME_STEP*theta_dot; // euler's method
+	// filter low freq noise out of gyro data
+	filtered_theta_g = march_filter(&HP,theta_g);
+
+    // calc theta from accelerometer G and Z components
+	g_y = data.accel[1]-0.1;  // Y direction is 0.1 too high
+	g_z = data.accel[2]-0.45; // Z direction is 0.45 too high
+	theta_a = atan2(-g_z/9.8,g_y/9.8); // angle to gravity
+	// filter high freq noise out of accelerometer data
+	filtered_theta_a = march_filter(&LP,theta_a);
+    
+    // add them togeter
+    sum = filtered_theta_a + filtered_theta_g;
+	// Print data to console
+	printf("%6.2f %6.2f %6.2f   |",	data.accel[0],\
+									data.accel[1],\
+									data.accel[2]);
+	printf("        %6.2f      |", filtered_theta_g); // Print angle from acc
+	printf("        %6.2f      |", filtered_theta_a); // Print angle from gyro
+	printf("        %6.2f      |", sum); // Print sum
+	
+    fprintf(fp,"%6.2f,%6.2f,%6.2f\n",filtered_theta_g,filtered_theta_a,sum); // print
+	
+    fflush(fp); // flush to file
+	fflush(stdout); // flush to console (?)
 	return 0;
 }
--- my_read_sensors/my_read_sensors.c
+++ my_read_sensors/my_read_sensors.c
@@ -1,20 +1,24 @@
 /*******************************************************************************
 * my_read_sensors.c
 *
-* Uses the DMP mode to print the accelerometer data to the console at 10hz
+* Use accelerometer and gyro data to print current angle of BBB
+* By: Stuart Sonatina
 *******************************************************************************/
 
 #include <usefulincludes.h>
 #include <roboticscape.h>
+
+#define SAMPLE_RATE 20
 
 // function declarations
 int initialize_imu_dmp(imu_data_t *data, imu_config_t imu_config);
 int set_imu_interrupt_func(int (*func)(void));
 int stop_imu_interrupt_func();
 int print_data(); //prints data to console
-	
+
 // variable declarations
 imu_data_t data; //struct to hold new data from IMU
+float g_y, g_z, theta_dot, theta_a; // gravity, thetas
 float theta_g = 0; // initialize starting angle for euler's method
 float offset = -0.5; // offset of gyro around X axis
 char filename[] = "HW5"; // file name for csv
@@ -23,27 +27,28 @@ char filename[] = "HW5"; // file name for csv
 int print_data(){
 	printf("\r ");
 
-	// Print accelerometer data
-    printf("%6.2f %6.2f %6.2f   |",	data.accel[0],\
+    // Integrate gyro data to get absolute position for gyro-derived theta
+	theta_dot = (data.gyro[0] - offset)*DEG_TO_RAD; // spin rate in rad
+	theta_g = theta_g + (1.0/(float)SAMPLE_RATE)*theta_dot; // euler's method
+
+    // calculate theta from accelerometer data
+	g_y = data.accel[1]-0.1;  // Y direction is 0.1 too high
+	g_z = data.accel[2]-0.45; // Z direction is 0.45 too high
+	theta_a = atan2(-g_z/9.8,g_y/9.8); // angle to gravity
+
+	// Print all data to console
+	printf("%6.2f %6.2f %6.2f   |",	data.accel[0],\
 									data.accel[1],\
-                  data.accel[2]);
-	float g_y = data.accel[1]-0.1;  // Y direction is 0.1 too high
-	float g_z = data.accel[2]-0.45; // Z direction is 0.45 too high
-	float theta_a = atan2(-g_z/9.8,g_y/9.8); // angle to gravity
-	printf("        %6.2f      ",-theta_a);
-	
-	// Integrate gyro data to get absolute position
-	float theta_dot = (data.gyro[0] - offset)*DEG_TO_RAD; // spin rate in rad
-	theta_g = theta_g + 0.05*theta_dot; // euler's method with t = 0.05
-	// Print angle from gyro data
-	printf("|    %6.1f    ", theta_g);
+									data.accel[2]);
+	printf("        %6.2f      |", theta_a);
+	printf("    %6.1f    ", theta_g);
 	
 	// print thetas to csv file
 	FILE *fp;
 	fp=fopen(filename,"a"); // open file to append
-	fprintf(fp,"%6.2f,%6.2f\n",theta_g,theta_a); // print to file
-	fclose(fp);
-	
+	fprintf(fp,"%6.2f,%6.2f\n",theta_g,theta_a); // print
+	fclose(fp); // close file
+
 	fflush(stdout); // flush
 	return 0;
 }
@@ -51,18 +56,21 @@ int print_data(){
 * int main()
 ******************************************************************************/
 int main(){
-  
+	printf("\n------------------------------");
+	printf("\n|   Welcome to Theta town!   |\n");
+	printf("------------------------------\n");
+	
 	// Initialize cape library
 	if(initialize_cape()){
 		printf("ERROR: failed to initialize_cape\n");
 		return -1;
 	}
-
+	
 	// set imu configuration to defaults
 	imu_config_t imu_config = get_default_imu_config();
-	
+    // Now adjust imu config
 	imu_config.orientation = ORIENTATION_Y_UP; // change orientation to Y up
-	imu_config.dmp_sample_rate = 20; // change sample rate
+	imu_config.dmp_sample_rate = SAMPLE_RATE;  // change sample rate
 	
 	if(initialize_imu_dmp(&data, imu_config)){
 		printf("initialize_imu_dmp() failed\n");
@@ -74,7 +82,6 @@ int main(){
 	// create CSV to record movements
 	printf("\nCreating %s.csv file\n",filename);
 	FILE *fp; // pointer to stream
-	//filename=strcat(filename,".csv"); // concat filename to .csv
 	fp=fopen(strcat(filename,".csv"),"w"); // create empty file to write
 	fprintf(fp,"theta_g,theta_a\n"); // print header to file
 	printf("%s.csv file created\n",filename);
